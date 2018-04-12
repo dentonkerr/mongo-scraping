d9500ddd0910b000fc6d00c065ff0ffe64451120
Denton Kerr <dentonkerr@gmail.com>
2018-04-12 17:52:04 -0400

    adds note function

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,25 @@
-$(document).on("click", "#scrape", function() {
+$(document).on("click", "#scrape", function () {
     console.log("test scrape click");
 
-    $.getJSON("/scrape", function(data) {
+    $.getJSON("/scrape", function (data) {
         // console.log(data)
-        for (var i =0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             $("#articles").append(
-                "<p data-id= " + data[i]._id + ">" + 
-                "Title: " + data[i].title + 
-                "<button type=button class=btn btn-danger id=addNote>Add Note</button>" + 
-                "<br />" + 
-                "<a href = " + data[i].link + ">Link</a>" + 
-                "<br />" + 
+                "<p data-id= " + data[i]._id + ">" +
+                "Title: " + data[i].title +
+                "<button type=button class=btn btn-danger id=addNote>Add Note</button>" +
+                "<br />" +
+                "<a href = " + data[i].link + ">Link</a>" +
+                "<br />" +
                 "Description: " + data[i].description +
+                "<br />" +
+                "_________________________" +
                 "</p>");
         }
     })
 });
 
-$(document).on("click", "#addNote", function() {
+$(document).on("click", "#addNote", function () {
     console.log("test note click");
 
     $("#notes").empty();
@@ -30,9 +32,16 @@ $(document).on("click", "#addNote", function() {
         method: "GET",
         url: "/articles/" + thisId
     })
-    .then(function(data) {
-        console.log(data);
-        $("#notes").append("<h2>" + data.title + "</h2>");
-    })
+        .then(function (data) {
+            console.log(data);
+            $("#notes").append("<h2>" + data.title + "</h2>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
+            if (data.note) {
+                $("#titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            }
+        });
 });
